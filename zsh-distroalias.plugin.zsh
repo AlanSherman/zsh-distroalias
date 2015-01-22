7b1a1a7e57eaa0177b45b0a9db84968789691114
Alan Sherman <alan.r.sherman@gmail.com>
2015-01-22 14:59:56 -0500

    update for better distro support

--- zsh-distroalias.plugin.zsh
+++ zsh-distroalias.plugin.zsh
@@ -1,15 +1,17 @@
 # checks /etc/issue and /etc/issue.net to for current distro
-if [ -n "`egrep -i 'Ubuntu|Debian|Mint' /etc/issue*`" ] ; then
+if [ -d /cygdrive] ; then
+  #best to cry 
+elif [ -n "`egrep -i 'Ubuntu|Debian|Mint' /etc/issue*`" ] ; then
   alias install='sudo apt-get install'
   alias remove='sudo apt-get remove'
   alias search='apt-cache search'
   alias update='sudo apt-get update'
   alias upgrade='sudo apt-get update && sudo apt-get upgrade'
-fi
-
-if [ -n "`egrep -i 'CentOS|Red Hat|Fedora' /etc/issue*`" ] ; then
+elif [ -n "`egrep -i 'CentOS|Red Hat|Fedora' /etc/issue*`" ] ; then
   alias install='sudo yum install'
   alias remove='sudo yum remove'
   alias search='yum search'
   alias update='sudo yum update'
+else
+  echo "distro not found by zsh-distroalias.plugin.zsh"
 fi

2d1dabc37b667f0f298eb380986f0e7feccfce61
Alan Sherman <alan.r.sherman@gmail.com>
2015-02-10 17:09:43 -0500

    get rid of error messages

--- zsh-distroalias.plugin.zsh
+++ zsh-distroalias.plugin.zsh
@@ -1,13 +1,13 @@
 # checks /etc/issue and /etc/issue.net to for current distro
 if [ -d /cygdrive ] ; then
   #best to cry 
-elif [ -n "`egrep -i 'Ubuntu|Debian|Mint' /etc/issue* /etc/*release*`" ] ; then
+elif [ -n "`egrep -si 'Ubuntu|Debian|Mint' /etc/issue* /etc/*release*`" ] ; then
   alias install='sudo apt-get install'
   alias remove='sudo apt-get remove'
   alias search='apt-cache search'
   alias update='sudo apt-get update'
   alias upgrade='sudo apt-get update && sudo apt-get upgrade'
-elif [ -n "`egrep -i 'CentOS|Red Hat|Fedora' /etc/issue* /etc/*release*`" ] ; then
+elif [ -n "`egrep -si 'CentOS|Red Hat|Fedora' /etc/issue* /etc/*release*`" ] ; then
   alias install='sudo yum install'
   alias remove='sudo yum remove'
   alias search='yum search'

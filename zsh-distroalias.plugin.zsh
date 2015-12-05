b7ea6c9fcae19273cd5e683ff8fe77151376d2f8
Alan Sherman <alansherman@alans-mbp.smarshcorp.com>
2015-12-04 16:41:49 -0800

    addded support for mac

--- zsh-distroalias.plugin.zsh
+++ zsh-distroalias.plugin.zsh
@@ -12,6 +12,11 @@ elif [ -n "`egrep -si 'CentOS|Red Hat|Fedora' /etc/issue* /etc/*release*`" ] ; t
   alias remove='sudo yum remove'
   alias search='yum search'
   alias update='sudo yum update'
+elif [ -n "`uname | grep -si 'Darwin'`" ] ; then
+  alias install='brew install'
+  alias remove='brew uninstall'
+  alias search='brew search'
+  alias update='brew update'
 else
   echo "distro not found by zsh-distroalias.plugin.zsh"
 fi
